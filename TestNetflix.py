3cec8d72add1254bf18a1fe1aa30a3dd2b6ab2ce
Omar Lalani <omarlalani@gmail.com>
2014-06-25 21:40:28 -0500

    All Unit Tests Passing (Average 3 per function) closes #6

--- TestNetflix.py
+++ TestNetflix.py
@@ -41,19 +41,61 @@ class TestNetflix (TestCase) :
     # ------
 
     def test_caches_1 (self) :
-    	self.assertEqual(Netflix.movie_stats_cache["12582"], [3.861750254033212, 3.954606433095186, 0.9326874144951447])
-    	self.assertEqual(Netflix.customer_stats_cache["341963"], [3.648, 3.7625, 1.0899981651360704])
-    	self.assertEqual(Netflix.answer_cache["12582-341963"], 3)
+        self.assertEqual(Netflix.movie_stats_cache["12582"], [3.861750254033212, 3.954606433095186, 0.9326874144951447])
+        self.assertEqual(Netflix.customer_stats_cache["341963"], [3.648, 3.7625, 1.0899981651360704])
+        self.assertEqual(Netflix.answer_cache["12582-341963"], 3)
+        self.assertEqual(Netflix.cust_ratings_by_decade["378466"], {'1990': 4.348178137651822, 
+                                                                    '1980': 4.591304347826087, 
+                                                                    '1940': 0, 
+                                                                    '1960': 4.769230769230769, 
+                                                                    '1970': 4.696969696969697, 
+                                                                    '1920': 0, 
+                                                                    '2000': 4.423357664233577, 
+                                                                    '1930': 0, 
+                                                                    '1950': 5.0, 
+                                                                    '1910': 0, 
+                                                                    '1890': 0, 
+                                                                    '1900': 0})
+        self.assertEqual(Netflix.movies_by_decade["1990"], 3.617264036843734)
+        self.assertEqual(Netflix.movie_info[12582], ('2003', 'Mystic River'))
 
     def test_caches_2 (self) :
-    	self.assertEqual(Netflix.movie_stats_cache["5318"], [3.595210220910109, 3.701846691344674, 1.1428533047409748])
-    	self.assertEqual(Netflix.customer_stats_cache["2009093"], [3.5, 4.0, 1.5811388300841898])
-    	self.assertEqual(Netflix.answer_cache["5318-2009093"], 1)
+        self.assertEqual(Netflix.movie_stats_cache["5318"], [3.595210220910109, 3.701846691344674, 1.1428533047409748])
+        self.assertEqual(Netflix.customer_stats_cache["2009093"], [3.5, 4.0, 1.5811388300841898])
+        self.assertEqual(Netflix.answer_cache["5318-2009093"], 1)
+        self.assertEqual(Netflix.cust_ratings_by_decade["778283"], {'1990': 3.5369127516778525, 
+                                                                    '1980': 3.792452830188679, 
+                                                                    '1940': 4.666666666666667, 
+                                                                    '1960': 4.4, 
+                                                                    '1970': 4.258620689655173, 
+                                                                    '1920': 0, 
+                                                                    '2000': 3.445273631840796, 
+                                                                    '1930': 4.5, 
+                                                                    '1950': 4.25, 
+                                                                    '1910': 0, 
+                                                                    '1890': 0, 
+                                                                    '1900': 0})
+        self.assertEqual(Netflix.movies_by_decade["2000"], 3.522762354036767)
+        self.assertEqual(Netflix.movie_info[5318], ('1995', 'Tommy Boy'))
 
     def test_caches_3 (self) :
-    	self.assertEqual(Netflix.movie_stats_cache["406"], [3.7627578830450252, 3.8279188072932406, 0.9271958105755236])
-    	self.assertEqual(Netflix.customer_stats_cache["1989674"], [3.7689530685920576, 3.954954954954955, 1.11958765680189])
-    	self.assertEqual(Netflix.answer_cache["406-1989674"], 2)
+        self.assertEqual(Netflix.movie_stats_cache["406"], [3.7627578830450252, 3.8279188072932406, 0.9271958105755236])
+        self.assertEqual(Netflix.customer_stats_cache["1989674"], [3.7689530685920576, 3.954954954954955, 1.11958765680189])
+        self.assertEqual(Netflix.answer_cache["406-1989674"], 2)
+        self.assertEqual(Netflix.cust_ratings_by_decade["585188"], {'1990': 4.0, 
+                                                                    '1980': 5.0, 
+                                                                    '1940': 4.0, 
+                                                                    '1960': 0, 
+                                                                    '1970': 4.0, 
+                                                                    '1920': 0, 
+                                                                    '2000': 3.5714285714285716, 
+                                                                    '1930': 4.333333333333333, 
+                                                                    '1950': 4.666666666666667, 
+                                                                    '1910': 0, 
+                                                                    '1890': 0, 
+                                                                    '1900': 0})
+        self.assertEqual(Netflix.movies_by_decade["1900"], 2.7889908256880735)
+        self.assertEqual(Netflix.movie_info[406], ('2005', 'Hostage'))
 
     # ----
     # read
@@ -93,15 +135,15 @@ class TestNetflix (TestCase) :
 
     def test_eval_1 (self) :
         v = Netflix.netflix_eval('2043', ['1417435', '2312054', '462685'])
-        self.assertEqual(v, [3.64324048327134, 4.117403851389367, 3.7850349544635087])
+        self.assertEqual(v, [4.0, 3.6, 4.0])
 
     def test_eval_2 (self) :
         v = Netflix.netflix_eval('12582', ['341963', '874390', '1842453', '441381'])
-        self.assertEqual(v, [3.7548751270166063, 3.705875127016606, 3.6150856533323954, 3.5975417936832725])
+        self.assertEqual(v, [4.0, 3.7, 3.6, 4.3])
 
     def test_eval_3 (self) :
         v = Netflix.netflix_eval('10851', ['2221448', '1703866', '2118663'])
-        self.assertEqual(v, [4.066092919115589, 3.6772040302267, 3.9512214974756086])
+        self.assertEqual(v, [5.0, 3.3, 4.3])
 
     # -------------
     # getMovieStats
@@ -135,7 +177,6 @@ class TestNetflix (TestCase) :
         customerStats = Netflix.netflix_getCustStats("126")
         self.assertEqual(customerStats, [4.673469387755102, 4.871794871794872, 0.7663258512456566])
 
-
     # -------------
     # getTrueRating
     # -------------
@@ -151,6 +192,69 @@ class TestNetflix (TestCase) :
     def test_getTrueRating_3 (self) :
         truth = Netflix.netflix_getTrueRating("406", ["1989674"])
         self.assertEqual(truth, [2])
+
+    # -----------------------
+    # netflix_getCustByDecade
+    # -----------------------
+
+    def test_getCustByDecade_1 (self) :
+        avgRating = Netflix.netflix_getCustByDecade("378466", 1920)
+        self.assertEqual(avgRating, 0)
+
+    def test_getCustByDecade_2 (self) :
+        avgRating = Netflix.netflix_getCustByDecade("778283", 1990)
+        self.assertEqual(avgRating, 3.5369127516778525)
+
+    def test_getCustByDecade_3 (self) :
+        avgRating = Netflix.netflix_getCustByDecade("585188", 1950)
+        self.assertEqual(avgRating, 4.666666666666667)
+
+    # ----------------------------
+    # netflix_getAvgRatingByDecade
+    # ----------------------------
+
+    def test_getAvgRatingByDecade_1 (self) :
+        avgRating = Netflix.netflix_getAvgRatingByDecade("1990")
+        self.assertEqual(avgRating, 3.617264036843734)
+
+    def test_getAvgRatingByDecade_2 (self) :
+        avgRating = Netflix.netflix_getAvgRatingByDecade("2000")
+        self.assertEqual(avgRating, 3.522762354036767)
+
+    def test_getAvgRatingByDecade_3 (self) :
+        avgRating = Netflix.netflix_getAvgRatingByDecade("1900")
+        self.assertEqual(avgRating, 2.7889908256880735)
+
+    # ------------------
+    # netflix_print_RMSE
+    # ------------------
+
+    def test_print_RMSE_1 (self) :
+        w = StringIO()
+        Netflix.sds = 6.0
+        Netflix.counter = 3
+        Netflix.netflix_print_RMSE(w)
+        self.assertEqual(w.getvalue(), "RMSE: 1.4142\n")
+        Netflix.sds = 0.0
+        Netflix.counter = 0
+
+    def test_print_RMSE_2 (self) :
+        w = StringIO()
+        Netflix.sds = 39.0
+        Netflix.counter = 9
+        Netflix.netflix_print_RMSE(w)
+        self.assertEqual(w.getvalue(), "RMSE: 2.0817\n")
+        Netflix.sds = 0.0
+        Netflix.counter = 0
+
+    def test_print_RMSE_3 (self) :
+        w = StringIO()
+        Netflix.sds = 20.0
+        Netflix.counter = 6
+        Netflix.netflix_print_RMSE(w)
+        self.assertEqual(w.getvalue(), "RMSE: 1.8257\n")
+        Netflix.sds = 0.0
+        Netflix.counter = 0
 
     # ------------------
     # netflix_update_sds
@@ -177,7 +281,7 @@ class TestNetflix (TestCase) :
         Netflix.sds = 0.0
         Netflix.counter = 0
 
-    def test_netflix_update_sds_2 (self) :
+    def test_netflix_update_sds_3 (self) :
         Netflix.sds = 0.0
         Netflix.counter = 0
         Netflix.netflix_update_sds([1, 2, 3], [3, 1, 4])
@@ -193,20 +297,24 @@ class TestNetflix (TestCase) :
         Netflix.counter = 0
 
     # -----------
-    # algorithm_1
+    # algorithm_7
     # -----------
 
-    def test_algorithm_1_a (self):
-        predicted = Netflix.netflix_algorithm_1('107', ['1611', '5399'])
-        self.assertEqual(predicted, [3.543953140433768, 3.5822712007585764])
+    def test_algorithm_7_1 (self):
+        predicted = Netflix.netflix_algorithm_7('107', ['1611', '5399'])
+        self.assertEqual(predicted, [3.6, 3.6])
 
-    def test_algorithm_1_b (self):
-        predicted = Netflix.netflix_algorithm_1('8776', ['1611', '5399'])
-        self.assertEqual(predicted, [3.2997900009843706, 3.338108061309179])
+    def test_algorithm_7_2 (self):
+        predicted = Netflix.netflix_algorithm_7('8776', ['1611', '5399'])
+        self.assertEqual(predicted, [3.0, 3.1])
 
-    def test_algorithm_1_c (self):
-        predicted = Netflix.netflix_algorithm_1('17770', ['126'])
-        self.assertEqual(predicted, [3.7449865939861287])
+    def test_algorithm_7_3 (self):
+        predicted = Netflix.netflix_algorithm_7('17770', ['126'])
+        self.assertEqual(predicted, [3.6])
+
+    def test_algorithm_7_4 (self):
+        predicted = Netflix.netflix_algorithm_7('4794', ['126'])
+        self.assertEqual(predicted, [4.1])
 
     # -----
     # print
@@ -235,13 +343,13 @@ class TestNetflix (TestCase) :
         r = StringIO("2043:\n1417435\n2312054\n462685\n")
         w = StringIO()
         Netflix.netflix_solve(r, w)
-        self.assertEqual(w.getvalue(), "2043:\n3.6\n4.1\n3.8\nRMSE: 1.84\n")
+        self.assertEqual(w.getvalue(), '2043:\n4.0\n3.6\n4.0\nRMSE: 1.6083\n')
 
     def test_solve_2 (self) :
         r = StringIO("2043:\n1417435\n2312054\n462685\n12582:\n341963\n874390\n1842453\n441381\n")
         w = StringIO()
         Netflix.netflix_solve(r, w)
-        self.assertEqual(w.getvalue(), "2043:\n3.6\n4.1\n3.8\n12582:\n3.8\n3.7\n3.6\n3.6\nRMSE: 1.46\n")
+        self.assertEqual(w.getvalue(), '2043:\n4.0\n3.6\n4.0\n12582:\n4.0\n3.7\n3.6\n4.3\nRMSE: 1.2985\n')
 
 # ----
 # main
